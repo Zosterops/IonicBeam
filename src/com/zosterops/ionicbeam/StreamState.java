a852572d82d0d232ba117a784a122958c5a0270f
thetlk <jeremieboutoille@gmail.com>
2015-03-10 18:56:18 +0100

    Initial commit

---
+++ src/com/zosterops/ionicbeam/StreamState.java
@@ -0,0 +1,8 @@
+package com.zosterops.ionicbeam;
+
+/**
+ * Created by rusty on 10/03/2015.
+ */
+public class StreamState implements Packet{
+
+}
---
+++ src/com/zosterops/ionicbeam/TrackingState.java
@@ -0,0 +1,7 @@
+package com.zosterops.ionicbeam;
+
+/**
+ * Created by rusty on 10/03/2015.
+ */
+public class TrackingState {
+}
